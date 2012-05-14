28d1e4f0507b1414b9e6d3a5ee174d6f5b1a9fef
Giovanni Capuano <webmaster@giovannicapuano.net>
2012-05-14 18:05:56 +0200

    GNU/GPLv3 added.

--- app/controllers/posts.rb
+++ app/controllers/posts.rb
@@ -1,3 +1,22 @@
+#--
+# Copyright(C) 2012 Giovanni Capuano <webmaster@giovannicapuano.net>
+#
+# This file is part of Dafuq.
+#
+# Dafuq is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Dafuq is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Dafuq.  If not, see <http://www.gnu.org/licenses/>.
+#++
+
 # Routes and application logic relatives to the posts.
 class Dafuq
 	default = :json # default format
--- dafuq.rb
+++ dafuq.rb
@@ -1,3 +1,22 @@
+#--
+# Copyright(C) 2012 Giovanni Capuano <webmaster@giovannicapuano.net>
+#
+# This file is part of Dafuq.
+#
+# Dafuq is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Dafuq is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Dafuq.  If not, see <http://www.gnu.org/licenses/>.
+#++
+
 require 'sinatra/base' # framework
 require 'data_mapper' # database
 require 'dm-sqlite-adapter' # sqlite database
@@ -9,8 +28,6 @@ require 'rack/csrf' # csrf protection
 class Dafuq < Sinatra::Base
   DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/db/database.db")
 	enable :sessions
-	
-	@title = 'Dafuq'
 	
   configure do
     set :method_override, true
